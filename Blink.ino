a58960da0174f9bfa31f128c47c1bba242f71f3a
YuriHenriqueRezende <yuri.henrique.rezende@outlook.com.br>
2023-09-01 19:20:44 -0300

    Create Blink.ino

---
+++ Blink.ino
@@ -0,0 +1,14 @@
+void setup() // configurações
+{
+  pinMode(2, OUTPUT); // ação
+}
+
+void loop() // laço infinito
+{
+  digitalWrite(2, HIGH);
+  delay(500);
+  digitalWrite(2, LOW);
+  delay(500);
+}
+
+
