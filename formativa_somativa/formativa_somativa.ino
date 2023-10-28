52b71d7056c8fafae1b83660c2e70c49bb79b7ef
YuriHenriqueRezende <yuri.henrique.rezende@outlook.com.br>
2023-10-27 23:29:15 -0300

    Update formativa_somativa.ino

--- formativa_somativa/formativa_somativa.ino
+++ formativa_somativa/formativa_somativa.ino
@@ -1,25 +1,22 @@
-#include <PubSubClient.h>
-#include <WiFi.h>
-#include "DHTesp.h"
-#include <Adafruit_Sensor.h>
-const char* ssid = "yuri";
-const char* password = "123456789";
-const char* mqtt_server = "test.mosquitto.org";
+#include <PubSubClient.h>                        // Essa biblioteca permite que dispositivos baseados em Arduino se conectem a sistemas de mensagens e publiquem/assinem tópicos em um sistema de mensagens. //
+#include <WiFi.h>                                // Essa biblioteca do Arduino que permite que dispositivos baseados em Arduino se conectem a redes Wi-Fi, possibilitando a comunicação com a Internet e outros dispositivos em uma rede local sem fio. //
+#include "DHTesp.h"                              // Essa biblioteca específica para trabalhar com sensores de temperatura e umidade da série DHT (como DHT11, DHT22, etc.) //
+const char* ssid = "yuri";                       // E o nome da minha rede WIFI que eu quero conectar ao ESP //
+const char* password = "123456789";              // A senha da minha rede wifi que eu quero conectar ao ESP //
+const char* mqtt_server = "test.mosquitto.org";  // Endereço do servidor MQTT real ao meu ESP. //
 
-#include "DHTesp.h"
+int RELE_PIN = 5;  // Estou definindo a porta do meu rele //
 
-int RELE_PIN = 5;
+const int DHT_PIN = 15;  // Estou definindo a porta do meu sensor de temperatura //
 
-const int DHT_PIN = 15;
+#define MSG_BUFFER_SIZE (50)  // Essa linha especifica o tamanho de um buffer ou array de mensagens, garantindo que o tamanho seja consistente em todo o programa. //
 
-#define MSG_BUFFER_SIZE (50)
+char msg[MSG_BUFFER_SIZE]; // Cria um array de caracteres chamado msg com um tamanho específico, determinado pela constante MSG_BUFFER_SIZE que foi definida anteriormente no código com #define MSG_BUFFER_SIZE (50). //
 
-char msg[MSG_BUFFER_SIZE];
-
-uint32_t delayMS;
-unsigned long lastMsg = 0;
-int value = 0;
-WiFiClient espClient;
+uint32_t delayMS; // Variável para controlar um atraso ou temporização em milissegundos. //
+unsigned long lastMsg = 0; // Variável que rastreia o tempo, em milissegundos ou outra unidade, na qual ocorreu a última ação ou evento. //
+int value = 0; // Armazenar valores inteiros e a inicializa com o valor 0. // 
+WiFiClient espClient; // Serve para se conectar a um servidor remoto e enviar ou receber dados pela rede. //
 PubSubClient client(espClient);
 
 DHTesp dhtSensor;
@@ -27,11 +24,11 @@ DHTesp dhtSensor;
 void setup() {
   Serial.begin(115200);
   dhtSensor.setup(DHT_PIN, DHTesp::DHT22);
-  sensor_t sensor;
   setup_WiFi();
   client.setServer(mqtt_server, 1883);
   pinMode(RELE_PIN, OUTPUT);
 }
+
 void setup_WiFi() {
   delay(10);
   Serial.println("");
@@ -44,7 +41,7 @@ void setup_WiFi() {
   }
   Serial.println("");
   Serial.print("WiFi conectando");
-  Serial.println("IP: ");  
+  Serial.println("IP: ");
   Serial.println(WiFi.localIP());
 }
 
@@ -56,20 +53,19 @@ void reconnect() {
     if (client.connect(clientId.c_str())) {
       Serial.println("conectando");
 
-      sensors_event_t event; // Declare event here
     } else {
       Serial.print("falhou, rc=");
       Serial.print(client.state());
       Serial.print("tente novamente em 5s");
-      delay(500);}
+      delay(500);
     }
+  }
 }
 
 void loop() {
   delay(delayMS);
-  sensors_event_t event;
-  TempAndHumidity  data = dhtSensor.getTempAndHumidity();
-  if (isnan(data.temperature)){
+  TempAndHumidity data = dhtSensor.getTempAndHumidity();
+  if (isnan(data.temperature)) {
     Serial.println(F(" ERRO NA LEITURA DA TEMPERATURA"));
   } else {
     Serial.print(F("TEMPERATURA:"));
@@ -78,7 +74,7 @@ void loop() {
     sprintf(msg, "%f", data.temperature);
     client.publish("yuri/temperature", msg);
 
-    if(data.temperature > 22) {
+    if (data.temperature > 22) {
       sprintf(msg, "%i", 1);
       client.publish("yuri/ligadotemp", msg);
     } else {
@@ -86,7 +82,7 @@ void loop() {
       client.publish("yuri/ligadotemp", msg);
     }
   }
-  if (isnan(data.humidity)){
+  if (isnan(data.humidity)) {
     Serial.println(F(" ERRO NA LEITURA DA HUMIDADE"));
   } else {
     Serial.print(F("HUMIDADE:"));
@@ -94,17 +90,17 @@ void loop() {
     Serial.println(F("%"));
     sprintf(msg, "%f", data.humidity);
     client.publish("yuri/humidity", msg);
-    
-    if(data.humidity > 50) {
+
+    if (data.humidity > 50) {
       sprintf(msg, "%i", 1);
       client.publish("yuri/ligar", msg);
     } else {
       sprintf(msg, "%i", 0);
-      client.publish("yuri/ligar", msg);  
+      client.publish("yuri/ligar", msg);
     }
   }
   delay(2000);
-  if(data.temperature >= 22){
+  if (data.temperature >= 22) {
     digitalWrite(RELE_PIN, HIGH);
     delay(5000);
   } else {
@@ -112,17 +108,13 @@ void loop() {
     delay(5000);
   }
 
-  if(data.temperature >= 22){
+  if (data.temperature >= 22) {
     digitalWrite(RELE_PIN, HIGH);
     delay(5000);
   } else {
     digitalWrite(RELE_PIN, LOW);
     delay(5000);
   }
-
-
-
-
 
   if (!client.connected()) {
     reconnect();
